1efc4a5d66b70dd1422c6f97ee8052022057b000
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-16 04:00:48 +0530

    Implement Nodejs Console Module Code !

---
+++ Console/app.js
@@ -0,0 +1,6 @@
+console.log('Hello Nodejs');
+console.log('Hello %s',"Nodejs");
+console.error('Error Spotted !');
+console.error(new Error('Hell! This is a wrong method.')); 
+const name = 'Sumit';  
+console.warn(`Don't mess with me ${name}! Don't mess with me!`);
