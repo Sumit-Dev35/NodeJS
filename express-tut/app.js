ff978e56d6aa5ab10c6abef96cce94b299196f63
Sumit Tiwari <sumitt.dev35@gmail.com>
2025-07-19 13:21:26 +0000

    Added express tut concept

--- express-tut/app.js
+++ express-tut/app.js
@@ -1,16 +1,28 @@
 const { log } = require('console');
-const http=require('http');
+const http = require('http');
+const {readFileSync}=require('fs');
 
-const server=http.createServer((req,res)=>{
-    log('user hit the server');
-    res.end('Hello World')
+const homePage=readFileSync(`./index.html`);
+console.log(homePage)
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    })
+    if (req.url === '/'){
+      res.write(homePage)
+    }
+    else if (req.url === '/about')
+        res.write(`<h1>About Page</h1>`)
+    res.end()
 })
 
-server.listen(5000,(error)=>{
-    if(error){
-        log(`Got an error`,err)
+server.listen(5000, (error) => {
+    if (error) {
+        log(`Got an error`, err)
     }
-    else{
+    else {
         log(`Server is listening on this ${5000}`)
     }
 })
